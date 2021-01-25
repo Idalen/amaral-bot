252222bffca9885361fc17368d8c31b0b889915f
Daniel <danielmv829@gmail.com>
2021-01-24 21:39:18 -0300

    Fix package

--- bot.js
+++ bot.js
@@ -2,11 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 require('dotenv/config');
-const ytdl = require('ytdl-core');
 
 const notBotChannel = require('./commands/notBotChannel');
 const deleteMessages = require('./commands/deleteMessages');
-const { link } = require('ffmpeg-static');
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
